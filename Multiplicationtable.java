4617565436fd05d3c5a22a25cd295c58a7bfdfd8
DerangulaDivya7 <divyaderangula7@gmail.com>
2025-04-19 06:05:47 +0530

    Create Multiplicationtable.java

---
+++ Multiplicationtable.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+   int  n=2;
+     for(int i=0;i<=10;i++){
+    System.out.println(n+"*"+(i+1)+"="+(n*(i+1)));
+     }
+ }
+ }
