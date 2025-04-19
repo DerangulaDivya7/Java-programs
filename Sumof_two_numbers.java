0230a402778a9ad1cef7a694f00df0200117949e
DerangulaDivya7 <divyaderangula7@gmail.com>
2025-04-19 06:07:14 +0530

    Create Sumof_two_numbers.java

---
+++ Sumof_two_numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter first value");
+    int n1=sc.nextInt();
+    System.out.println("enter second value");
+    int n2=sc.nextInt();
+    int total=n1+n2;
+    System.out.println(total);
+  }
+}
